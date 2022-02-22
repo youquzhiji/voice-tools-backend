bf983b435e9657e389794d85d0f016f7a3891f38
Hykilpikonna <me@hydev.org>
2022-02-22 16:47:12 -0500

    [O] Store token

--- src/constants.py
+++ src/constants.py
@@ -1,4 +1,5 @@
 
 
+token_path = './config/token.txt'
 coordinator_host = 'localhost:8000'
 version = 1
--- src/server/utils.py
+++ src/server/utils.py
@@ -1,12 +1,24 @@
 import random
 import string
+from pathlib import Path
 
-from constants import version
+from constants import version, token_path
 
 
-def generate_token(length: int = 2048):
+def generate_token(length: int = 2048) -> str:
     return ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(length))
 
 
+def load_token() -> str:
+    path = Path(token_path)
+    if path.is_file():
+        return path.read_text('UTF-8').strip()
+    else:
+        token = generate_token()
+        path.parent.mkdir(parents=True, exist_ok=True)
+        path.write_text(token, 'UTF-8')
+        return load_token()
+
+
 def get_server_info():
-    return {'token': generate_token(), 'version': version}
+    return {'token': load_token(), 'version': version}
