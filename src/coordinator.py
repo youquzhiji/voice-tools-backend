ffcddd22614bb29adea3524264c1eaa7d36aee5d
Hykilpikonna <me@hydev.org>
2022-02-22 16:08:57 -0500

    [+] Websockets

--- src/coordinator.py
+++ src/coordinator.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 from tortoise.contrib.fastapi import register_tortoise
 
+db_url = 'mysql://pwp:qwq@localhost:3306/6414'
 app = FastAPI()
 
 
@@ -9,10 +10,23 @@ async def root():
     return {'message': 'Hello World'}
 
 
+@app.get('/lifecycle')
+async def lifecycle():
+    return {'message': 'To be implemented'}
+
+
+@app.websocket('/ws/server-connect')
+async def server_connect(ws: WebSocket):
+    await ws.accept()
+    while True:
+        data = await ws.receive_text()
+        await ws.send_text(f"Message text was: {data}")
+
+
 # Register Tortoise Database
 register_tortoise(
     app,
-    db_url='mysql://pwp:qwq@localhost:3306/test',
+    db_url=db_url,
     modules={'models': ['db']},
     generate_schemas=True,
     add_exception_handlers=True
--- src/db.py
+++ src/db.py
@@ -4,5 +4,13 @@ from tortoise import fields
 
 class Server(Model):
     id = fields.IntField(pk=True)
-    token = fields.CharField(max_length=255)
+    token = fields.CharField(max_length=2048)
+
+    # Only approved servers can be used
+    approved = fields.BooleanField(default=False)
+
+    # Whether we fully trust the server
     trusted = fields.BooleanField(default=False)
+
+    # Creation date
+    created = fields.DatetimeField()
