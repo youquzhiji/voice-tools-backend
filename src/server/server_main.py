cbd80311947c5a798e39eab219cf0974957c8ee9
Hykilpikonna <me@hydev.org>
2022-02-22 17:15:30 -0500

    [+] Automatic reconnection

--- src/server/server_main.py
+++ src/server/server_main.py
@@ -2,6 +2,7 @@ import asyncio
 import json
 
 import websockets
+from websockets.exceptions import ConnectionClosedError
 from websockets.legacy.client import WebSocketClientProtocol
 
 from constants import coordinator_host
@@ -16,8 +17,21 @@ async def start():
         await ws.send(json.dumps(get_server_info()))
 
         # Receive validation results
-        print(await ws.recv())
+        validation = await ws.recv()
+        if validation.strip() != 'Success':
+            raise ConnectionError(validation)
+        print('[+] Connected')
+
+        while True:
+            msg = await ws.recv()
+            print(f'> Received message: {msg}')
+            await ws.send(json.dumps({'success': True}))
 
 
 if __name__ == '__main__':
-    asyncio.run(start())
+    while True:
+        try:
+            asyncio.run(start())
+        except (ConnectionClosedError, TimeoutError) as e:
+            print(f'[-] Connection closed, reconnecting... ({str(e)})')
+            continue
