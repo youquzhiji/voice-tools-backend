d49eca02fd8a0d95dd74dd3ed276a44988ef559e
Hykilpikonna <me@hydev.org>
2022-02-23 15:53:56 -0500

    [+] Server queue

--- src/coordinator/run.py
+++ src/coordinator/run.py
@@ -1,7 +1,10 @@
 import asyncio
 import json
 import re
+import uuid
 from dataclasses import dataclass
+from functools import reduce
+from typing import Callable
 
 from fastapi import FastAPI, WebSocket
 from starlette.websockets import WebSocketState
@@ -37,9 +40,20 @@ class ConnectedServer:
     max_tasks: int
 
 
+@dataclass()
+class Task:
+    id: str
+    task: str
+    params: any
+    callback: Callable
+
+
 class ServerPool:
     """This class controls server pools and keeps information about each server"""
     pool: list[ConnectedServer] = []
+    completed_tasks = []
+    running_tasks: dict[str, tuple[Task, ConnectedServer]] = {}
+    queued_tasks: list[Task] = []
 
     def remove_disconnected(self) -> None:
         to_remove = [s for s in self.pool if s.ws.client_state == WebSocketState.DISCONNECTED]
@@ -49,6 +63,48 @@ class ServerPool:
     def get_connected(self) -> list[ConnectedServer]:
         self.remove_disconnected()
         return self.pool
+
+    def get_resting_servers(self) -> list[ConnectedServer]:
+        """Get servers that are not doing anything. If a server has a max_tasks of 6 and is resting,
+        then the server will appear in the result 6 times.
+        """
+        active = [s[1] for s in self.running_tasks.values()]
+        resting = [[s] * (s.max_tasks - active.count(s)) for s in self.get_connected()]
+        return reduce(list.__add__, resting) if resting else []
+
+    async def check_queue(self):
+        """Check if any tasks in queue can be started"""
+        resting = self.get_resting_servers()
+
+        # Run tasks
+        while len(resting) > 0 and len(self.queued_tasks) > 0:
+            s = resting.pop(0)
+            t = self.queued_tasks.pop(0)
+
+            # Run and add to running list
+            await s.ws.send_json({'type': 'compute', 'id': t.id, 'task': t.task, 'params': t.params})
+            self.running_tasks[t.id] = (t, s)
+
+    async def run_compute(self, task: str, params, callback: Callable) -> None:
+        """Enqueue a computation task"""
+        id = str(uuid.uuid4())
+        self.queued_tasks.append(Task(id, task, params, callback))
+        await self.check_queue()
+
+    async def add_server(self, server: ConnectedServer):
+        """Listen to server finishing requests"""
+        self.pool.append(server)
+
+        async def listen():
+            try:
+                while server.ws.client_state == WebSocketState.CONNECTED:
+                    text = await server.ws.receive_text()
+                    print(text)
+            except ConnectionClosedError:
+                print(f'> [-] {server.ws.client.host} Connection closed')
+                return
+
+        await asyncio.gather(listen())
 
 
 pool = ServerPool()
@@ -61,10 +117,7 @@ async def root():
 
 @app.get('/test')
 async def endpoint_test():
-    # Get unclosed servers
-    target = pool.get_connected()[0].ws
-    await target.send_json({'type': 'compute'})
-    return await target.receive_text()
+    await pool.run_compute('pi', 1000, print)
 
 
 @app.get('/pool')
@@ -101,7 +154,7 @@ async def server_connect(ws: WebSocket):
         # Passed, add to server pool
         print('> [+] Validation passed.')
         await ws.send_text('Success')
-        pool.pool.append(ConnectedServer(info, server, ws, max_tasks))
+        await pool.add_server(ConnectedServer(info, server, ws, max_tasks))
 
     # Any other errors
     except Exception as e:
@@ -110,17 +163,6 @@ async def server_connect(ws: WebSocket):
             await ws.send_text(f'Error: {str(e)}')
         return
 
-    # Send ping https://github.com/tiangolo/fastapi/issues/709
-    async def ping():
-        try:
-            while ws.client_state == WebSocketState.CONNECTED:
-                await ws.send_text('1')
-                await asyncio.sleep(1)
-        except ConnectionClosedError:
-            print(f'> [-] {server.nickname} Connection closed')
-            return
-
-    await asyncio.gather(ping())
 
 # Register Tortoise Database
 register_tortoise(
